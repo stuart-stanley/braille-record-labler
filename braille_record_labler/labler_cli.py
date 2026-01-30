36586ea640999539cf81d2119bc0110272f4c585
Stuart Stanley <stuart@glowforge.com>
2026-01-30 15:40:11 -0800

    sketch out cli structure

--- braille_record_labler/labler_cli.py
+++ braille_record_labler/labler_cli.py
@@ -1,5 +1,38 @@
 import click
 
 
-def labeler_cli():
-    print("hello")
+@click.group('braille-record-labler')
+@click.pass_context
+@click.option('--lp-database-file', default='lp_database.yml')
+@click.option('--config-ish-file')
+def braille_record_labler(ctx, lp_database_file, config_ish_file):
+    print("rl", lp_database_file, config_ish_file, ctx)
+    ctx.obj = {
+        'lp_database': lp_database_file,
+        'config_ish': config_ish_file
+    }
+
+
+@braille_record_labler.command()
+@click.pass_context
+def list(ctx):
+    print("listing records", ctx.obj)
+
+
+@braille_record_labler.group()
+def lp():
+    print("in lp subcommands")
+
+
+@lp.command()
+@click.argument('lp_index_name')
+@click.pass_context
+def validate(ctx, lp_index_name):
+    print("validate xyz", lp_index_name, ctx.obj)
+
+
+@lp.command()
+@click.argument('lp_index_name')
+@click.pass_context
+def generate(ctx, lp_index_name):
+    print("generate", lp_index_name, ctx.obj)
